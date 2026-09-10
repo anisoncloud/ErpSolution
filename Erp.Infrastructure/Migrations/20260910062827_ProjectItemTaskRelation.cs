965cd49e1287f00e37a21f9cc92561da1c67f7f3
Anisur Rahman <anisoncloud@gmail.com>
2026-09-10 15:51:23 +0600

    ProjectItem Index

---
+++ Erp.Infrastructure/Migrations/20260910062827_ProjectItemTaskRelation.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Erp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectItemTaskRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTasks_ProjectItems_ProjectItemId",
+                schema: "tpm",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectTasks_ProjectItemId",
+                schema: "tpm",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectItemId",
+                schema: "tpm",
+                table: "ProjectTasks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTasks_ProjectId",
+                schema: "tpm",
+                table: "ProjectTasks",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTasks_ProjectItems_ProjectId",
+                schema: "tpm",
+                table: "ProjectTasks",
+                column: "ProjectId",
+                principalSchema: "tpm",
+                principalTable: "ProjectItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTasks_ProjectItems_ProjectId",
+                schema: "tpm",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectTasks_ProjectId",
+                schema: "tpm",
+                table: "ProjectTasks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectItemId",
+                schema: "tpm",
+                table: "ProjectTasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTasks_ProjectItemId",
+                schema: "tpm",
+                table: "ProjectTasks",
+                column: "ProjectItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTasks_ProjectItems_ProjectItemId",
+                schema: "tpm",
+                table: "ProjectTasks",
+                column: "ProjectItemId",
+                principalSchema: "tpm",
+                principalTable: "ProjectItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
